1839bbe7271970d2932e5d97f0c550e70d9ee2d2
Less <46290638+Lessism@users.noreply.github.com>
2019-02-25 17:48:01 +0900

    Roster club list

--- DAO/EditDAO.java
+++ DAO/EditDAO.java
@@ -16,9 +16,19 @@ public class EditDAO {
 //	Roster list
 	
 	public Map<String, Object> listRoster(Map<String, Object> map) {
-		
+
 		map.put("roster", db.selectList("FIFA.roster_list", map));
-		map.put("list", db.selectList("FIFA.list", map));
+		map.put("role", "Club");
+		
+		if (map.get("name") == null) {
+			map.put("club", db.selectList("FIFA.list", map));
+		}
+		else {
+			map.replace("role", "Manager");
+			map.put("manager", db.selectList("FIFA.list", map));
+			map.replace("role", "Player");
+			map.put("player", db.selectList("FIFA.list", map));
+		}
 		
 		return map;
 	}
