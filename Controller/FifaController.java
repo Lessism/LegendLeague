16719ae7e1cce2481716d890ae9d193b05e9eb5c
Less <46290638+Lessism@users.noreply.github.com>
2019-02-27 17:52:42 +0900

    Add files via upload

--- Controller/FifaController.java
+++ Controller/FifaController.java
@@ -52,7 +52,7 @@ public class FifaController {
 			if (!img.getFile("img").isEmpty()) {
 				map.put("img", img.getFile("img").getBytes());
 			}
-			if (map.get("role") == "Club" && !img.getFile("img").isEmpty()) {
+			if (map.get("role").equals("Club") && !img.getFile("img").isEmpty()) {
 				map.put("img1", img.getFile("img1").getBytes());
 			}
 			fDAO.join(map);
