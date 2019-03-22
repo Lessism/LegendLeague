e935db41cec75a3a49cb83ae302374d22d136c2f
Less <46290638+Lessism@users.noreply.github.com>
2019-03-22 17:51:06 +0900

    FIFA Award

--- DAO/FifaDAO.java
+++ DAO/FifaDAO.java
@@ -1,6 +1,7 @@
 package com.lessism.legendleague.dao;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -26,6 +27,61 @@ public class FifaDAO {
 	
 		public int join(Map<String, Object> map) {
 			return db.insert("FIFA.join", map);
+		}
+	
+	
+//	Award
+	
+		public Map<String, Object> award(Map<String, Object> map) {
+
+			Map<String, Object> award = new HashMap<>(map);
+			
+			if (award.get("role").equals("Club")) {
+
+				Map<String, Object> champion = db.selectOne("FIFA.award", award);
+				champion.replace("manager", db.selectOne("FIFA.info_manager", champion.get("manager")));
+				champion.put("role", "Player");
+				champion.put("lineup", db.selectList("League.lineup", champion));
+				
+				award.put("champion", champion);
+				award.put("list", db.selectList("League.award", award));
+				
+			} else if (award.get("role").equals("Manager")) {
+				
+				Map<String, Object> manager = db.selectOne("FIFA.award", award);
+				manager.put("odds", db.selectOne("FIFA.award_odds", manager));
+				
+				award.put("manager", manager);
+				award.put("list", db.selectList("FIFA.award_list", award));
+				
+				
+			} else if (award.get("role").equals("Player")) {
+				
+				Map<String, Object> ballonDor = db.selectOne("FIFA.award_ballondor", award);
+				ballonDor.put("score", db.selectOne("FIFA.award_score", ballonDor));
+				ballonDor.put("odds", db.selectOne("FIFA.award_odds", ballonDor));
+				ballonDor.put("list", db.selectList("FIFA.award_list", ballonDor));
+				
+				Map<String, Object> goalScorer = db.selectOne("FIFA.award_goalScorer", award);
+				goalScorer.put("score", db.selectOne("FIFA.award_score", goalScorer));
+				goalScorer.put("odds", db.selectOne("FIFA.award_odds", goalScorer));
+				goalScorer.put("list", db.selectList("FIFA.award_list", goalScorer));
+				
+				Map<String, Object> assistProvider = db.selectOne("FIFA.award_assistProvider", award);
+				assistProvider.put("score", db.selectOne("FIFA.award_score", assistProvider));
+				assistProvider.put("odds", db.selectOne("FIFA.award_odds", assistProvider));
+				assistProvider.put("list", db.selectList("FIFA.award_list", assistProvider));
+
+				award.put("ballonDor", ballonDor);
+				award.put("goalScorer", goalScorer);
+				award.put("assistProvider", assistProvider);
+				
+			}
+
+			map.put("season", db.selectOne("League.recency_season"));
+			map.put("award", award);
+			
+			return map;
 		}
 	
 	
