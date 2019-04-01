64547039446cd66d6d00cce5e41c0d99567407cc
Less <46290638+Lessism@users.noreply.github.com>
2019-04-01 14:14:18 +0900

    Master Visit

--- DAO/MasterDAO.java
+++ DAO/MasterDAO.java
@@ -14,13 +14,14 @@ public class MasterDAO {
 	private SqlSession db;
 	
 	
-//	Visit
+//	Visit Total
 	
-		public Map<String, Object> visit(Map<String, Object> map) {
+		public Map<String, Object> total(Map<String, Object> map) {
 			
 			Map<String, Object> visit = new HashMap<>(map);
 			visit.put("total", db.selectOne("Master.visit_total"));
 			visit.put("today", db.selectOne("Master.visit_today"));
+			visit.put("listtotal", db.selectOne("Master.visit_list_total"));
 			
 			map.put("visit", visit);
 			
@@ -32,9 +33,36 @@ public class MasterDAO {
 		
 		public Map<String, Object> date(Map<String, Object> map) {
 			
-			Map<String, Object> date = new HashMap<>(map);
+			Map<String, Object> visit = new HashMap<>(map);
+			visit.put("list", db.selectList("Master.visit_list"));
+			visit.put("avgtotal", db.selectOne("Master.visit_avg_total"));
 			
-			map.put("date", date);
+			map.put("visit", visit);
+			
+			return map;
+		}
+		
+		
+//	Visit Menu
+			
+		public Map<String, Object> menu(Map<String, Object> map) {
+			
+			Map<String, Object> visit = new HashMap<>(map);
+			
+			visit.put("type", "master");
+			visit.put("master", db.selectOne("Master.visit_stay_menu", visit));
+			visit.put("mastersub", db.selectList("Master.visit_stay_submenu", visit));
+			visit.put("type", "account");
+			visit.put("account", db.selectOne("Master.visit_stay_menu", visit));
+			visit.put("accountsub", db.selectList("Master.visit_stay_submenu", visit));
+			visit.put("type", "league");
+			visit.put("league", db.selectOne("Master.visit_stay_menu", visit));
+			visit.put("leaguesub", db.selectList("Master.visit_stay_submenu", visit));
+			visit.put("type", "fifa");
+			visit.put("fifa", db.selectOne("Master.visit_stay_menu", visit));
+			visit.put("fifasub", db.selectList("Master.visit_stay_submenu", visit));
+			
+			map.put("visit", visit);
 			
 			return map;
 		}
